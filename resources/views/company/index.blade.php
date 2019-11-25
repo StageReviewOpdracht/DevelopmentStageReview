41c10bf81de67cc170894d1c55301052cae2f665
MSI\Jordy Clement <99044408@mydavinci.nl>
2019-11-25 11:34:29 +0100

    start from reviews

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -16,17 +16,16 @@ $data = CompanyController::index();
                 <div class="card-body">
                         <div class="form-group row">
                             <div class="col-md-6">
-                                <h1>Test Page</h1>
+                                <h1><strong>Companies</strong></h1>
                                 <table>
                                     <tr>
-                                        <th><h1>Bedrijfsnaam:</h1></th>
-                                        <th><h1>Locatie:</h1></th>
+                                        <th><h3>Companies</h3></th>
+                                        <th><h3>Locations</h3></th>
                                     </tr>
                                     @foreach($data as $company)
                                     <tr>
-                                        <td>{{ $company->name }}</td>
+                                        <td><a href="{{action('CompanyController@show', ['id' => $company->id])}}">{{ $company->name }}</a></td>
                                         <td>{{ $company->location }}</td>
-                                        <td><a class="btn btn-success" href="{{action('CompanyController@show', ['id' => $company->id])}}">View</a></td>
                                     </tr>
                                     @endforeach
                                 </table>
--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -5,16 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <nav class="nav">
-		            <a class="nav-link" href="{{ URL::to('companyindex') }}">Back to all the companies</a>
-	            </nav>
-                <div class="card-header"></div>
+                <div class="card-header">
+                    <a class="nav-link" href="{{ URL::to('companyindex') }}">Back to all the companies</a>
+                </div>
                 <div class="card-body">
                     <div class="form-group row">
                         <div class="col-md-6">
                             <h1>Company Page</h1>
-                            <p><strong>Bedrijfsnaam:</strong> {{ $company->name }}</p>
-                            <p><strong>Locatie:</strong> {{ $company->location }}</p>
+                            <p><strong>Companies:</strong> {{ $company->name }}</p>
+                            <p><strong>Locations:</strong> {{ $company->location }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                        <h3>Review</h3>
                         </div>
                     </div>
                 </div>
